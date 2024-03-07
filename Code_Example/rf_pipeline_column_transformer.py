ca7591f454e1b1b64f7fb457bf791d7e9cb9a538
Yifan Wu <34715745+yifanwu0909@users.noreply.github.com>
2024-03-07 12:23:53 -0600

    Create rf_pipeline_column_transformer.py

---
+++ Code_Example/rf_pipeline_column_transformer.py
@@ -0,0 +1,60 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+
+label_counts = df['target'].value_counts()
+missing_percentage = df.isnull().mean() * 100
+#or sort it
+missing_percentage = df.isnull().mean().round(4).mul(100).sort_values(ascending=False)
+df.dtypes
+df.info()
+df.dtypes.to_dict()
+
+num_pipeline = Pipeline(steps=[
+    ('impute', SimpleImputer(strategy='mean')),
+    ('scale', StandardScaler())
+])
+
+cat_pipeline = Pipeline(steps=[
+    ('impute', SimpleImputer(strategy='constant', fill_value=0)),
+    ('encode', OneHotEncoder(handle_unknown='ignore'))
+])
+
+num_columns = df.select_dtypes(include=['float', 'int']).columns
+cat_columns = df.select_dtypes(include=['object']).column
+
+preprocessor = ColumnTransformer(transformers=[
+    ('num', num_pipeline, num_columns),
+    ('cat', cat_pipeline, cat_columns),
+    ('drop_cat_feature_drop', 'drop', ['cat_feature_drop'])  # Dropping the column
+])
+
+# Final pipeline including the classifier
+pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('classifier', RandomForestClassifier(random_state=42))
+])
+
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(X_df, y, test_size=0.2, random_state=42)
+
+# Train the model
+pipeline.fit(X_train, y_train)
+
+cv_scores = cross_val_score(model_pipeline, X_trainval, y_trainval, cv=5, scoring='accuracy')
+
+# Evaluate the model
+print(f"Model accuracy on test set: {pipeline.score(X_test, y_test):.4f}")
+
+# Define Stratified K-Fold cross-validation
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+scores = cross_val_score(pipeline, X_trainval, y_trainval, cv=skf, scoring='accuracy')
+print("Cross-validation scores:", scores)
+print("Mean accuracy:", scores.mean())
+print("Standard deviation of accuracy:", scores.std())
